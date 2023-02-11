485bac46f954153764bc541576ef7c8b7c30fa5a
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2023-02-11 19:34:14 +0200

    Create 01.Sum_Matrix_Elements.cs

---
+++ 02.01. Multidimensional Arrays - Lab/01.Sum_Matrix_Elements.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SoftUni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int[,] ints = new int[size[0], size[1]];
+            for (int i = 0; i < ints.GetLength(0); i++)
+            {
+               int[] numsOfRow = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+                for (int j = 0; j < ints.GetLength(1); j++)
+                {
+                    ints[i, j] = numsOfRow[j];
+                }
+            }
+            int sum = 0;
+            Console.WriteLine(ints.GetLength(0));
+            Console.WriteLine(ints.GetLength(1));
+            for (int i = 0; i < ints.GetLength(1); i++)
+            {
+                for (int j = 0; j < ints.GetLength(0); j++)
+                {
+                    sum += ints[j, i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
